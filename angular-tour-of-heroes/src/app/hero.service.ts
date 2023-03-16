54e8850e2cc052cff01965be4a0dbee181dcbae6
Jellios <62890668+Jellios@users.noreply.github.com>
2023-03-16 08:40:06 +0100

    tot nieuwe tap getheroes

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -3,22 +3,55 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) {}
-  getHeroes(): Observable<Hero[]> {
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+    ) {}
+  /*getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
     this.messageService.add('Heroservice: fetched heroes');
     return heroes;
+  } */
+  getHeroes(): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        tap(_ => this.log('fetched heroes')),
+        catchError(this.handleError<Hero[]>('getHeroes', []))
+      );
   }
-  getHero(id: number): Observable<Hero> {
+  /*getHero(id: number): Observable<Hero> {
 
     const hero = HEROES.find(h => h.id === id)!;
     this.messageService.add('HeroService: fetched hero id=' +id+ '');
     return of(hero);
+  } */
+  getHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
   }
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  private handleError<T>(operation = 'operation',result?: T)
+  {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      this.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    };
+  }
+
+  private heroesUrl = 'api/heroes';
 }
