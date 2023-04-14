74e39e55b125f246fd9643b6d36cf1e5257ca9dc
Jellios <62890668+Jellios@users.noreply.github.com>
2023-04-14 10:30:01 +0200

    d

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { GoalComponent } from './goal/goal.component';
 import { GoalsComponent } from './goals/goals.component';
 import { GoalFormComponent } from './goal-form/goal-form.component';
 import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,7 +16,8 @@ import { FormsModule } from '@angular/forms';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    FontAwesomeModule
 
   ],
   providers: [],
