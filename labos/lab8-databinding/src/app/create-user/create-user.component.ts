7111a4337ee31acf2b53d088336b9582c6977981
Jellios <62890668+Jellios@users.noreply.github.com>
2023-04-27 14:58:00 +0200

    beging routing

--- labos/lab8-databinding/src/app/create-user/create-user.component.ts
+++ labos/lab8-databinding/src/app/create-user/create-user.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-create-user',
@@ -6,8 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-user.component.css']
 })
 export class CreateUserComponent implements OnInit {
-  username:string = "";
-  password:string = "";
+ @Input() username:string = "";
+ @Input() password:string= "";
 ngOnInit(): void {
 
 }
--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -7,17 +7,26 @@ import { GoalsComponent } from './goals/goals.component';
 import { GoalFormComponent } from './goal-form/goal-form.component';
 import { FormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { RouterModule } from '@angular/router';
+import { NavigationComponent } from './navigation/navigation.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 @NgModule({
   declarations: [
     AppComponent,
     GoalComponent,
     GoalsComponent,
-    GoalFormComponent
+    GoalFormComponent,
+    NavigationComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    RouterModule.forRoot([
+      {path: 'dashboard',component: GoalsComponent},
+      {path: 'new-goal',component: GoalFormComponent},
+    ]),
 
   ],
   providers: [],
