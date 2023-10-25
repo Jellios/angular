d13376b26aa57c14cf0f1537a4adb693e870e648
Jellios <62890668+Jellios@users.noreply.github.com>
2023-10-25 13:02:41 +0200

    signup werkend

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { initializeApp, provideFirebaseApp} from '@angular/fire/app'
 import {getFirestore, provideFirestore} from '@angular/fire/firestore'
@@ -35,7 +35,8 @@ const appRoutes = [
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideFirestore(() => getFirestore()),
     provideAuth(() => getAuth()),
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- project/src/app/auth/signup/signup.component.ts
+++ project/src/app/auth/signup/signup.component.ts
@@ -1,18 +1,18 @@
-import { Component } from '@angular/core';
+import {Component , OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
-import {NgForm} from '@angular/forms'
 import { Router } from '@angular/router';
-
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent {
-constructor (private authService: AuthService,private router: Router) {}
+export class SignupComponent implements OnInit {
+constructor (private authService: AuthService,private router: Router, private fb: FormBuilder) {}
 
-onSignup(form: NgForm)
+/*onSignup(form: NgForm)
 {
   const email = form.value.email;
   const password = form.value.passwd;
@@ -25,5 +25,48 @@ onSignup(form: NgForm)
       alert(res);
     }
   })
+}  */
+form!: FormGroup;
+//emailTaken: boolean = false;
+ngOnInit(): void {
+  
+  this.form = this.fb.group({
+    'email': [null],
+    'passwd': [null]
+  });
+
+  /*const emailControl = this.form.get('email')
+   if (emailControl) {
+    emailControl.valueChanges.subscribe(async (email) => {
+      const res = await this.authService.isEmailTaken(email);
+      if (res) {
+        console.log('Email is already taken.');
+        this.emailTaken = true
+        console.log("taken");
+        // You can also show a message to the user or mark the email field as invalid here
+      }
+      else
+      {
+        this.emailTaken = false;
+      }
+    });
+}*/
+} 
+
+
+onSignup(): void {
+  const email = this.form.value['email'];
+  const password = this.form.value['passwd'];
+  this.authService.signup(email,password)
+  .then((res)=> {
+    if (res == 'success') {
+      this.router.navigate(['login']);
+    }
+    else {
+      alert(res);
+    }
+  })
 }
+
 }
+
