487f9be826b8d3c2039453484aba88fc04bd8cdb
Jellios <62890668+Jellios@users.noreply.github.com>
2023-11-02 16:03:52 +0100

    nav fixed

--- project/src/app/admin/admin-routing.module.ts
+++ project/src/app/admin/admin-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin.component';
+import { UserOverviewComponent } from './user-overview/user-overview.component';
 
-const routes: Routes = [{ path: '', component: AdminComponent }];
+const routes: Routes = [
+{
+  path: 'overview', component: UserOverviewComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- project/src/app/admin/admin.module.ts
+++ project/src/app/admin/admin.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import { UserOverviewComponent } from './user-overview/user-overview.component';
 import { UserDetailsComponent } from './user-details/user-details.component';
+import { TimersService } from '../timers/timers.service';
+import { AuthService } from '../auth/auth.service';
 
 
 @NgModule({
@@ -16,6 +18,8 @@ import { UserDetailsComponent } from './user-details/user-details.component';
   imports: [
     CommonModule,
     AdminRoutingModule
-  ]
+  ],
+  providers: [TimersService, AuthService,DatePipe]
+  
 })
 export class AdminModule { }
